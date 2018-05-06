5e9b612a6cf306bd66c10a2d9f37ca6a599d2943
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-06 21:06:27 +0300

    Adding cell component

--- foo/src/app/table-forms/table-forms.module.ts
+++ foo/src/app/table-forms/table-forms.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
 import { RowComponent } from './row/row.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TableComponent, RowComponent]
+  declarations: [TableComponent, RowComponent, CellComponent]
 })
 export class TableFormsModule { }
