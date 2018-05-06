ea549bfc8bfa91f26c150e394513dcb1ded156cc
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-06 21:05:55 +0300

    Adding row component

--- foo/src/app/table-forms/table-forms.module.ts
+++ foo/src/app/table-forms/table-forms.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
+import { RowComponent } from './row/row.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TableComponent]
+  declarations: [TableComponent, RowComponent]
 })
 export class TableFormsModule { }
