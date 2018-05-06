b348de7637ee4ba46b48b963ec0b44cd683a920c
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-06 21:19:58 +0300

    Moving components from declarations in TableFormsModule to an constant

--- foo/src/app/table-forms/table-forms.module.ts
+++ foo/src/app/table-forms/table-forms.module.ts
@@ -4,10 +4,15 @@ import { TableComponent } from './table/table.component';
 import { RowComponent } from './row/row.component';
 import { CellComponent } from './cell/cell.component';
 
+const components = [
+  TableComponent, RowComponent, CellComponent
+];
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TableComponent, RowComponent, CellComponent]
+  declarations: [...components],
+  exports: [...components]
 })
 export class TableFormsModule { }
