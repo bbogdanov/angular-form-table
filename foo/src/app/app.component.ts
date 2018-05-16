e40f94f9d263d4bfa342f05fae1cde4672cd40bd
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-16 23:17:57 +0300

    Updating app component for better testing environment

--- foo/src/app/app.component.ts
+++ foo/src/app/app.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {CellControl} from './table-forms/cell/cell';
+import {cellValueExist} from './table-forms/cell/validators/cellValueExist.validator';
 import {RowControl} from './table-forms/row/row';
+import {TableControl} from './table-forms/table/table';
+import {TableComponent} from './table-forms/table/table.component';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +11,68 @@ import {RowControl} from './table-forms/row/row';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild(TableComponent) table: TableComponent;
 
   public config = {
     rows: [new RowControl({
-      'test'     : new CellControl(''),
-      'test_two' : new CellControl(''),
-      'test_tree': new CellControl('')
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
     }), new RowControl({
-      'test'     : new CellControl(''),
-      'test_two' : new CellControl(''),
-      'test_tree': new CellControl('')
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
     }), new RowControl({
-      'test'      : new CellControl(''),
-      'test_two'  : new CellControl(''),
-      'test_three': new CellControl('')
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+    }), new RowControl({
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+    }), new RowControl({
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+    }), new RowControl({
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+    }), new RowControl({
+      'test'      : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test2'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test3'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test4'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
+      'test5'     : new CellControl('', cellValueExist({ valueExist: 'Value already exist.'})),
     })]
   };
+
+  public log() {
+    this.logger(this.table.table);
+  }
+
+  private logger(control: TableControl | RowControl) {
+    if (!control) return;
+
+    console.log(control);
+
+    if (control.controls) {
+      for (let index in control.controls) {
+        this.logger(control.controls[index]);
+      }
+    }
+  }
 
 }
