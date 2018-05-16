dc28087e6d912e05381bb195e4e65a3b4eb96b46
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-16 23:16:27 +0300

    Adding cell validator for already existing value in the table

---
+++ foo/src/app/table-forms/cell/validators/cellValueExist.validator.ts
@@ -0,0 +1,27 @@
+import {ValidationErrors, ValidatorFn} from '@angular/forms';
+import {RowControl} from '../../row/row';
+import {CellControl} from '../cell';
+
+export const cellValueExist = (errors: ValidationErrors): ValidatorFn => {
+  return (cell: CellControl): ValidationErrors | null => {
+    let validationErrors = null;
+
+    if (!cell.value) return validationErrors;
+
+    if (!cell.value && cell.pristine) return validationErrors;
+
+    const table = cell.getTable();
+
+    table.controls.forEach((row: RowControl) => {
+      for (let cellKey in row.controls) {
+        const currentCell = row.controls[cellKey];
+
+        if (currentCell !== cell && cell.value === currentCell.value) {
+          validationErrors = errors;
+        }
+      }
+    });
+
+    return validationErrors;
+  };
+};
