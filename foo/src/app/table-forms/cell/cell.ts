3859ccef69340e88911a6cdf0c2a7136702d8f96
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-16 23:22:48 +0300

    Adding getTable function in cell control and findTable recursive function

--- foo/src/app/table-forms/cell/cell.ts
+++ foo/src/app/table-forms/cell/cell.ts
@@ -1,13 +1,27 @@
-import {FormControl} from '@angular/forms';
+import {AbstractControl, FormControl} from '@angular/forms';
 import {AsyncValidatorFn, ValidatorFn} from '@angular/forms/src/directives/validators';
 import {AbstractControlOptions} from '@angular/forms/src/model';
+import {TableControl} from '../table/table';
 
 export class CellControl extends FormControl {
 
   public constructor(formState?: any,
                      validatorOrOpts?: ValidatorFn | ValidatorFn[] | AbstractControlOptions | null,
                      asyncValidator?: AsyncValidatorFn | AsyncValidatorFn[] | null) {
-    super(formState, validatorOrOpts, asyncValidator)
+    super(formState, validatorOrOpts, asyncValidator);
+  }
+
+  public getTable(): TableControl {
+    return this.findTable(this.parent);
+  }
+
+  //TODO move that one to utils function.
+  private findTable(parent: AbstractControl): TableControl {
+    if (parent instanceof TableControl) {
+      return parent;
+    } else {
+      return this.findTable(parent.parent);
+    }
   }
 
 }
