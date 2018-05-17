a1b85096bc475c72645c28a0e55580e95bd4581b
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-17 09:36:00 +0300

    Adding validation for parameter value in findTable

--- foo/src/app/utils/findTable.ts
+++ foo/src/app/utils/findTable.ts
@@ -2,6 +2,8 @@ import {AbstractControl} from '@angular/forms';
 import {TableControl} from '../table-forms/table/table';
 
 export const findTable = (parent: AbstractControl): TableControl => {
+  if (!parent) return;
+
   if (parent instanceof TableControl) {
     return parent;
   } else {
