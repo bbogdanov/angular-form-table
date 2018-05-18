99545b3005c932ef462bcf6e0ed49da5f0c71738
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-18 17:35:59 +0300

    Removing foo folder and package-lock.json, updating LICENSE file

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: 'foo',
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
