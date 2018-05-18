a031a030828ca3cfa1fc329d9b60785d470dc076
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-05-18 18:18:15 +0300

    Adding validation function for entire table in TableControl

--- src/app/table-forms/table/tableControl.ts
+++ src/app/table-forms/table/tableControl.ts
@@ -1,6 +1,7 @@
 import {FormArray} from '@angular/forms';
 import {AsyncValidatorFn, ValidatorFn} from '@angular/forms/src/directives/validators';
 import {AbstractControl, AbstractControlOptions} from '@angular/forms/src/model';
+import {RowControl} from '../row/rowControl';
 
 export class TableControl extends FormArray {
 
@@ -18,10 +19,34 @@ export class TableControl extends FormArray {
    * @param {number} index The position of the row in the table.
    */
   public pop(index?: number) {
-    if (!index) this.removeAt(this.controls.length - 1);
+    if (!index) {
+      this.removeAt(this.controls.length - 1);
+    }
 
     if (index || index === 0) {
       this.removeAt(index);
+    }
+  }
+
+  /**
+   * Updates and validates the entire table.
+   *
+   * Table > rows > row > cells
+   */
+  public updateAndValidateAll() {
+    this.updateValueAndValidity();
+
+    if (this.controls.length > 0) {
+      this.controls.forEach((row: RowControl) => {
+        row.updateValueAndValidity();
+
+        if (Object.keys(row.controls).length > 0) {
+          for (let cellKey in row.controls) {
+            row.controls[cellKey].updateValueAndValidity();
+            row.controls[cellKey].updateValueAndValidity();
+          }
+        }
+      });
     }
   }
 
